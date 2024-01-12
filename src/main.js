9d5021541873b95501652574c169347568e16675
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2024-01-12 13:06:28 -0500

    THE ALPHABET ADDS UP TO 26!

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/* calculator guesser
+calculator gui
+1. Generate a random #
+2. user enters an expression 
+3. calculate if expresson === random # 
+4. store results 
+5. restart 
+
+IF USER WANTS TO CHECK HISTORY ITERATE THRU RESULTS */
+const results = []
+
+function simulation(results) {
+  
+}
