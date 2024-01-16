e9741567371879ac35802a49349dcae2e127bab7
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2024-01-16 13:12:39 -0500

    ASDKFJASLFJASUIFJASDKLFJL;ASD

--- src/main.js
+++ src/main.js
@@ -7,8 +7,24 @@ calculator gui
 5. restart 
 
 IF USER WANTS TO CHECK HISTORY ITERATE THRU RESULTS */
+const dom = {
+    numInput: document.getElementById('number'),
+    buttons: document.querySelectorAll('.input'),
+}
 const results = []
 
 function simulation(results) {
-  
+    function getRandomIntInclusive(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min); 
+      }
+    const randomNum = getRandomIntInclusive(1,18)
+    console.log(randomNum)
+    for(let i = 0; i < dom.buttons.length; i++) {
+        console.log(dom.buttons[i].innerHTML)
+        dom.buttons[i].addEventListener('click', (e) => {
+            dom.numInput.innerText = dom.numInput.innerText + dom.buttons[i].innerHTML
+        })
+    }
 }
+
+simulation(results)
