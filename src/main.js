86eede2e978ec2897c5ea966759725ac86144800
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2024-01-18 13:12:55 -0500

    progress

--- src/main.js
+++ src/main.js
@@ -1,42 +1,41 @@
-/* calculator guesser
-calculator gui
-1. Generate a random #
-2. user enters an expression 
-3. calculate if expresson === random # 
-4. store results 
-5. restart 
-
-IF USER WANTS TO CHECK HISTORY ITERATE THRU RESULTS */
 const dom = {
   numInput: document.getElementById("number"),
   buttons: document.querySelectorAll(".input"),
   randomNum: document.getElementById("randomNum"),
   clear: document.getElementById("clear"),
+  submit: document.getElementById("submit"),
 };
 const results = [];
 function checkProblem(num, input) {
-inputNum = eval()
+  let inputNum = eval(input);
+  dom.randomNum.innerHTML = dom.randomNum.innerHTML.replace('?', inputNum)
+  console.log(inputNum);
+  if (num === inputNum) {
+    console.log("GOOD JOB ");
+    dom.randomNum.innerHTML += ' TRUE'
+  } else {
+    console.log("GO BACK TO 5THG RADE");
+    om.randomNum.innerHTML += ' FALSE'
+  }
 }
 function simulation(results) {
-  const operation = [];
   function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
-  dom.randomNum.innerHTML = getRandomIntInclusive(1, 18);
+  const randomNum = getRandomIntInclusive(1, 18);
+  dom.randomNum.innerHTML = randomNum + ' = ?';
   for (let i = 0; i < dom.buttons.length; i++) {
     console.log(dom.buttons[i].innerHTML);
     dom.buttons[i].addEventListener("click", (e) => {
       dom.numInput.innerText =
         dom.numInput.innerText + dom.buttons[i].innerHTML;
-      if (operation.length === 3) {
-        checkProblem(num, operation)
-      } else {
-        operation.push(dom.buttons[i].innerHTML);
-      }
     });
   }
   dom.clear.addEventListener("click", (e) => {
     dom.numInput.innerText = "";
+  });
+  dom.submit.addEventListener("click", (e) => {
+    checkProblem(randomNum, dom.numInput.innerText);
   });
 }
 
