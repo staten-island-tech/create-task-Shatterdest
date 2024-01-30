2b5f2f7fac3d0f9262e7551acf670e2c5a920cde
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2024-01-30 13:16:42 -0500

    2 more steps left...!

--- src/main.js
+++ src/main.js
@@ -4,18 +4,43 @@ const dom = {
   randomNum: document.getElementById("randomNum"),
   clear: document.getElementById("clear"),
   submit: document.getElementById("submit"),
+  results: document.getElementById("results"),
 };
 const results = [];
+function removeListeners() {
+  dom.buttons.forEach((item) => {
+    item.removeEventListener("click", (e) => {
+      dom.numInput.innerText =
+        dom.numInput.innerText + dom.buttons[i].innerHTML;
+    });
+  });
+  dom.clear.removeEventListener("click", (e) => {
+    dom.numInput.innerText = "";
+  });
+  document.submit.removeEventListener("click", (e) => {
+    checkProblem(randomNum, dom.numInput.innerText);
+  });
+}
 function checkProblem(num, input) {
   let inputNum = eval(input);
-  dom.randomNum.innerHTML = dom.randomNum.innerHTML.replace('?', inputNum)
+  dom.randomNum.innerHTML = dom.randomNum.innerHTML.replace("?", inputNum);
   console.log(inputNum);
   if (num === inputNum) {
     console.log("GOOD JOB ");
-    dom.randomNum.innerHTML += ' TRUE'
+    dom.randomNum.innerHTML += " TRUE";
+    dom.results.insertAdjacentHTML(
+      "beforeend",
+      `<h1>${dom.randomNum.innerHTML}</h1>`
+    );
+    simulation(results);
   } else {
     console.log("GO BACK TO 5THG RADE");
-    om.randomNum.innerHTML += ' FALSE'
+    dom.randomNum.innerHTML += " FALSE";
+    dom.results.insertAdjacentHTML(
+      "beforeend",
+      `<h1>${dom.randomNum.innerHTML}</h1>`
+    );
+    simulation(results);
   }
 }
 function simulation(results) {
@@ -23,14 +48,13 @@ function simulation(results) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   const randomNum = getRandomIntInclusive(1, 18);
-  dom.randomNum.innerHTML = randomNum + ' = ?';
-  for (let i = 0; i < dom.buttons.length; i++) {
-    console.log(dom.buttons[i].innerHTML);
-    dom.buttons[i].addEventListener("click", (e) => {
+  dom.randomNum.innerHTML = randomNum + " = ?";
+  dom.buttons.forEach((item) => {
+    item.addEventListener("click", (e) => {
       dom.numInput.innerText =
         dom.numInput.innerText + dom.buttons[i].innerHTML;
     });
-  }
+  });
   dom.clear.addEventListener("click", (e) => {
     dom.numInput.innerText = "";
   });
