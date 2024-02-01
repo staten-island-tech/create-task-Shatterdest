ba69730175ce433976bbee7c575e94d88cac5f55
yi <73318855+Shatterdest@users.noreply.github.com>
2024-01-31 22:49:53 -0500

    rewrite

--- src/main.js
+++ src/main.js
@@ -8,46 +8,54 @@ const dom = {
 };
 const results = [];
 
-function checkProblem(num, input) {
-  let inputNum = eval(input);
-  dom.randomNum.innerHTML = dom.randomNum.innerHTML.replace("?", inputNum);
-  console.log(inputNum, num);
-  if (num === inputNum) {
-    console.log("GOOD JOB ");
-    dom.randomNum.innerHTML += " TRUE";
-    dom.results.insertAdjacentHTML(
-      "beforeend",
-      `<h1>${dom.randomNum.innerHTML}</h1>`
-    );
-    simulation(results);
-  } else {
-    console.log("GO BACK TO 5THG RADE");
-    dom.randomNum.innerHTML += " FALSE";
-    dom.results.insertAdjacentHTML(
-      "beforeend",
-      `<h1>${dom.randomNum.innerHTML}</h1>`
-    );
-    simulation(results);
-  }
-}
 function simulation(results) {
+  let randomNum;
+  dom.buttons.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      dom.numInput.innerText = dom.numInput.innerText + item.innerHTML;
+    });
+  });
+  dom.clear.addEventListener("click", (e) => {
+    dom.numInput.innerText = "";
+  });
+  dom.submit.addEventListener("click", (e) => {
+    checkProblem(randomNum, dom.numInput.innerText);
+  });
   function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
-  const randomNum = getRandomIntInclusive(1, 18);
-  dom.randomNumber.innerHTML = randomNum + " = ?";
+  function newNumber() {
+    const num = getRandomIntInclusive(1, 18);
+    dom.randomNumber.innerHTML = num + " = ?";
+    randomNum = num;
+  }
+  newNumber();
+
+  function checkProblem(num, input) {
+    let inputNum = eval(input);
+    dom.randomNumber.innerHTML = dom.randomNumber.innerHTML.replace(
+      "?",
+      inputNum
+    );
+    console.log(inputNum, num);
+    if (num === inputNum) {
+      console.log("GOOD JOB ");
+      dom.randomNumber.innerHTML += " TRUE";
+      dom.results.insertAdjacentHTML(
+        "beforeend",
+        `<h1>${dom.randomNumber.innerHTML}</h1>`
+      );
+      newNumber();
+    } else {
+      console.log("GO BACK TO 5THG RADE");
+      dom.randomNumber.innerHTML += " FALSE";
+      dom.results.insertAdjacentHTML(
+        "beforeend",
+        `<h1>${dom.randomNumber.innerHTML}</h1>`
+      );
+      newNumber();
+    }
+  }
 }
 
-
 simulation(results);
-dom.buttons.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    dom.numInput.innerText = dom.numInput.innerText + item.innerHTML;
-  });
-});
-dom.clear.addEventListener("click", (e) => {
-  dom.numInput.innerText = "";
-});
-dom.submit.addEventListener("click", (e) => {
-  checkProblem(randomNum, dom.numInput.innerText);
-});
